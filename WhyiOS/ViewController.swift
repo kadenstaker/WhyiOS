b9eb95136ff03f9f2b246f035ade0706cdc8c5cd
Kaden Staker <kadenstaker@kadens-macbook-pro.local>
2018-10-17 13:40:10 -0600

    Initial Commit

---
+++ WhyiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhyiOS
+//
+//  Created by Kaden Staker on 10/17/18.
+//  Copyright © 2018 Kaden Staker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
